cddb60e9bb827354ced001741db6a4fd7921353f
konstandik <52689504+konstandik@users.noreply.github.com>
2025-02-27 01:50:35 +0800

    Component installed

---
+++ local/components/native/test_component/component_info.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arComponentDescription = [
+    "NAME" => "Тестовый компонент",
+    "DESCRIPTION" => "Демонстрационный компонент для выполнения задания",
+    "ICON" => "",
+    "PATH" => [
+        "ID" => "custom_components",
+        "NAME" => "Кастомные компоненты",
+    ],
+];
